547abee9ba36c269bd0a4b13fbc141d5db742e2e
mson90906250 <l90906250@gmail.com>
2019-02-05 22:30:40 +0800

    在Main中將兩個container_a,container_b換成兩個fragment

--- fragment_otherfragment/src/main/java/tw/tcnr01/fragment_otherfragment/Main.java
+++ fragment_otherfragment/src/main/java/tw/tcnr01/fragment_otherfragment/Main.java
@@ -4,10 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
+    FragmentA fragmentA;
+    FragmentB fragmentB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentA = new FragmentA();
+        fragmentB = new FragmentB();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container_a,fragmentA)
+                .replace(R.id.container_b,fragmentB)
+                .commit();
     }
 }
