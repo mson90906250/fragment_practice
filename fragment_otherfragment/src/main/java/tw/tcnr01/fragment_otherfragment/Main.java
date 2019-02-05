e65bb712fe8bccdd697750f680c31a9278a391dc
mson90906250 <l90906250@gmail.com>
2019-02-05 22:36:44 +0800

    使Main implements 兩個Listener 並改寫其中的方法

--- fragment_otherfragment/src/main/java/tw/tcnr01/fragment_otherfragment/Main.java
+++ fragment_otherfragment/src/main/java/tw/tcnr01/fragment_otherfragment/Main.java
@@ -2,8 +2,8 @@ package tw.tcnr01.fragment_otherfragment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-public class Main extends AppCompatActivity {
+//讓Main implements 兩個Listener
+public class Main extends AppCompatActivity implements FragmentA.FragmentAListener, FragmentB.FragmentBListener {
     FragmentA fragmentA;
     FragmentB fragmentB;
 
@@ -19,5 +19,17 @@ public class Main extends AppCompatActivity {
                 .replace(R.id.container_a,fragmentA)
                 .replace(R.id.container_b,fragmentB)
                 .commit();
+    }
+
+    //將fragmentA裡的editText的值傳入fragmentB
+    @Override
+    public void inputASent(CharSequence input) {
+        fragmentB.updateEditText(input);
+    }
+
+    //將fragmentB裡的editText的值傳入fragmentA
+    @Override
+    public void inputBSent(CharSequence input) {
+        fragmentA.updateEditText(input);
     }
 }
