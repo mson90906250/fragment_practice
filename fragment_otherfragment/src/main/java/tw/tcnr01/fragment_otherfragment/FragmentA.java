718583002fe3a14ed680e523f2a941c675148a47
mson90906250 <l90906250@gmail.com>
2019-02-05 22:00:16 +0800

    新增兩個layout及一個class

---
+++ fragment_otherfragment/src/main/java/tw/tcnr01/fragment_otherfragment/FragmentA.java
@@ -0,0 +1,27 @@
+package tw.tcnr01.fragment_otherfragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FragmentA extends Fragment {
+    private EditText editText;
+    private Button buttonOk;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_a,container,false);
+
+        editText = v.findViewById(R.id.edit_text);
+        buttonOk = v.findViewById(R.id.button_ok);
+
+        return  v;
+    }
+}
