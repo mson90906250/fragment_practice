5a101c8f22bfc6a9e41c0af6c16e7dcc5ffe580d
mson90906250 <l90906250@gmail.com>
2019-02-05 21:05:08 +0800

    將data帶入fragment的方法(2) 將前一次的方式做些變動 使程式碼容易維護

--- fragment01/src/main/java/tw/tcnr01/fragment/ExampleFragment.java
+++ fragment01/src/main/java/tw/tcnr01/fragment/ExampleFragment.java
@@ -12,6 +12,10 @@ import android.widget.TextView;
 import tw.tcnr01.fragment.R;
 
 public class ExampleFragment extends Fragment {
+    //設定bundlez中的key值
+    private static final  String ARG_TEXT = "argText";
+    private static   final  String ARG_NUMBER = "argNumber";
+
     private  String text;
     private  int number;
 
@@ -24,12 +28,22 @@ public class ExampleFragment extends Fragment {
         //接收data
         //先判斷是否有data
         if (getArguments()!=null){
-            text = getArguments().getString("argText");
-            number = getArguments().getInt("argNumber");
+            text = getArguments().getString(ARG_TEXT);
+            number = getArguments().getInt(ARG_NUMBER);
 
             textView.setText(text+number);
         }
 
         return  v;
+    }
+
+    //由於系統不鼓勵使用建構元來作初始化,所以要透過自定一個方法來回傳一個ExampleFragment的物件
+    public static ExampleFragment newInstance(String text,int number){
+        ExampleFragment fragment = new ExampleFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TEXT,text);
+        args.putInt(ARG_NUMBER,number);
+        fragment.setArguments(args);
+        return fragment;
     }
 }
--- fragment01/src/main/java/tw/tcnr01/fragment/MainActivity.java
+++ fragment01/src/main/java/tw/tcnr01/fragment/MainActivity.java
@@ -10,12 +10,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ExampleFragment fragment = new ExampleFragment();
+        /*ExampleFragment fragment = new ExampleFragment();
         //將data帶入fragment-------方法一
+        //缺點會使程式碼變得非常雜亂,難以維護
         Bundle args = new Bundle();
         args.putString("argText","Hello World");
         args.putInt("argNumber",123);
-        fragment.setArguments(args);
+        fragment.setArguments(args);*/
+
+        //方法二 ,使用自定義的方法newInstance(String text,int number)來將data帶入並回傳一個ExampleFragment
+        ExampleFragment fragment = ExampleFragment.newInstance("Hello World",456);
 
         //將R.id.container換成fragment
         getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
